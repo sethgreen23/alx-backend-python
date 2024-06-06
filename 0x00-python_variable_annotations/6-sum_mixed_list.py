f7a95b7655ae92221d160fc427d48786cd738b8b
Dridi Chaith <dridi.chaith@gmail.com>
2024-06-06 16:41:44 +0100

    task 6

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -2,6 +2,7 @@
 """Module for adding two float variables"""
 from typing import Union, List
 
+
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """return the sum of list of integer and floats"""
     sum = 0
