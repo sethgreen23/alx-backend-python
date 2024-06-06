bca7484e658e75aa0e45bb8243e7de9c8f306f0c
Dridi Chaith <dridi.chaith@gmail.com>
2024-06-06 21:33:29 +0100

    task 0

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -4,6 +4,7 @@ from typing import List, Tuple
 
 
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """Function to anotate"""
     zoomed_in: List = [
         item for item in lst
         for i in range(factor)
