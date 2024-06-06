b75877ef4a82e3eac61eb68fe4d649007443e73d
Dridi Chaith <dridi.chaith@gmail.com>
2024-06-06 15:58:30 +0100

    task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module for returning a string represention of a float"""
+
+
+def to_str(a: float) -> str:
+    """return string representation of a float"""
+    return str(a)
