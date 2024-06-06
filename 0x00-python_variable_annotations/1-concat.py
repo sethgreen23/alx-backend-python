93ec861ddecde7ad5d5b2afa8995eca0d85ac05e
Dridi Chaith <dridi.chaith@gmail.com>
2024-06-06 15:19:38 +0100

    task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module for concatenating two strings"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Function to concatenate two strings"""
+    return str1 + str2
