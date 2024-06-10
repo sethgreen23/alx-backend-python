c6ce8e10a4f48eefd9b06cd5e6d9f1d9c1262f34
Dridi Chaith <dridi.chaith@gmail.com>
2024-06-10 22:42:19 +0100

    task 2

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,2 +1,14 @@
 #!/usr/bin/env python3
 """Asynchronous coroutine"""
+import time
+import asyncio
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """Measure the total existence time for wait_random n times"""
+    start = time.perf_counter()
+    asyncio.run(wait_n(n, max_delay))
+    end = time.perf_counter()
+    return (end - start) / n
