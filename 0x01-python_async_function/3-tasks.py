19865b6b63b5373b99324f9e0b0929e2a43801e9
Dridi Chaith <dridi.chaith@gmail.com>
2024-06-10 23:08:47 +0100

    task 3

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,2 +1,13 @@
 #!/usr/bin/env python3
 """Asynchronous coroutine"""
+import asyncio
+import random
+import time
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """Return a asyncio.Task"""
+    return asyncio.create_task(wait_random(max_delay))
