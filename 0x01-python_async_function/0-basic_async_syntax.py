34e1f77ba0622a6872cfc7de67b2a01dd1e0050f
Dridi Chaith <dridi.chaith@gmail.com>
2024-06-10 20:39:34 +0100

    task 0

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -9,4 +9,4 @@ async def wait_random(max_delay=10):
     """Asyncronous coroutine"""
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
-    return max_delay
+    return delay
