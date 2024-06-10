9bb812c755463d3cc652e4ff4297e525157c718d
Dridi Chaith <dridi.chaith@gmail.com>
2024-06-10 23:36:02 +0100

    task 4

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,2 +1,21 @@
 #!/usr/bin/env python3
 """Asynchronous coroutine"""
+
+#!/usr/bin/env python3
+"""Module to Execute multiple Coroutines at the same time"""
+
+import asyncio
+import time
+from typing import List
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """Execute multiple coroutines at the same time"""
+    lis = []
+    for i in range(n):
+        res = await wait_random(max_delay)
+        lis.append(res)
+    return sorted(lis)
