16bafc6fd43fd134066c6a03e3f7fa0d3c412d9c
Dridi Chaith <dridi.chaith@gmail.com>
2024-07-01 04:18:43 +0100

    task 0

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -7,7 +7,7 @@ from typing import Mapping, Sequence, Any, Dict
 from utils import (access_nested_map,
                    get_json,
                    memoize)
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from client import GithubOrgClient
 from fixtures import TEST_PAYLOAD
 
@@ -15,13 +15,25 @@ from fixtures import TEST_PAYLOAD
 class TestGithubOrgClient(unittest.TestCase):
     """Test github org client."""
     @parameterized.expand([
-		("google", {'google': True}),
-		("abc", {'abc': True}),
-	])
+        ("google", {'google': True}),
+        ("abc", {'abc': True}),
+        ])
     @patch('client.get_json')
-    def test_org(self, org_name: str, expected: dict, mocked_get_json: Any) -> None:
+    def test_org(self, org_name: str,
+                 expected: dict,
+                 mocked_get_json: Any) -> None:
         """Test org"""
         mocked_get_json.return_value = expected
         client_object = GithubOrgClient(org_name)
         self.assertEqual(client_object.org, expected)
-        mocked_get_json.assert_called_once_with(GithubOrgClient.ORG_URL.format(org=org_name))
+        mocked_get_json.assert_called_once_with(
+            GithubOrgClient.ORG_URL.format(org=org_name))
+
+    def test_public_repos_url(self) -> None:
+        """Test public repos url"""
+        expected = {'repos_url': 'google'}
+        with patch("client.GithubOrgClient.org",
+                   new_callable=PropertyMock) as mock_org:
+            client_object = GithubOrgClient("google")
+            mock_org.return_value = expected
+            self.assertEqual(client_object._public_repos_url, 'google')
